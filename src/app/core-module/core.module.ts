d619ce1343d37be66870ef970ee3c8a8c0a4f1af
Kamil <bizkamiladamski@gmail.com>
2021-06-18 15:05:28 +0200

    small upgrade with menu

--- src/app/core-module/core.module.ts
+++ src/app/core-module/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[SidebarComponent],
   declarations: [SidebarComponent]

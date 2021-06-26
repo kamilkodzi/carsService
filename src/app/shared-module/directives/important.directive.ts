0c48013c841288e286e57c1e05a0e8938cd17a66
Kamil <bizkamiladamski@gmail.com>
2021-06-26 10:35:06 +0200

    Add own directive to style some tabel data

---
+++ src/app/shared-module/directives/important.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[csImportant]'
+})
+export class ImportantDirective {
+
+  constructor(private el:ElementRef ) { 
+    el.nativeElement.style.fontWeight=700;
+    el.nativeElement.style.color="#295089";
+  }
+
+
+}
--- src/app/shared-module/shared.module.ts
+++ src/app/shared-module/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SurnameShortcutPipe } from './pipes/surname-shortcut.pipe';
+import { ImportantDirective } from './directives/important.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  exports:[HeaderComponent,SurnameShortcutPipe],
-  declarations: [HeaderComponent, SurnameShortcutPipe]
+  exports:[HeaderComponent,SurnameShortcutPipe,ImportantDirective],
+  declarations: [HeaderComponent, SurnameShortcutPipe, ImportantDirective]
 })
 export class SharedModule { }
